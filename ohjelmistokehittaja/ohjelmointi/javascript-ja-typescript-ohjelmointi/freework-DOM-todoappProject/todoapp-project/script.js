9b76f8130f09257a7eebe5327ba3d95e9c8890c2
metin-gercek <metin.gercek@edu.bc.fi>
2021-12-26 15:04:42 +0200

    DOM to do app project working with Local Storage

--- ohjelmistokehittaja/ohjelmointi/javascript-ja-typescript-ohjelmointi/freework-DOM-todoappProject/todoapp-project/script.js
+++ ohjelmistokehittaja/ohjelmointi/javascript-ja-typescript-ohjelmointi/freework-DOM-todoappProject/todoapp-project/script.js
@@ -1,14 +1,18 @@
-// UI vars
+// UI vars 
 
 const form = document.querySelector('form');
 const input = document.querySelector('#txtTaskName');
 const btnDeleteAll = document.querySelector('#btnDeleteAll');
 const taskList = document.querySelector('#task-list');
+let items;
 
+// load items
+loadItems();
 
-//call event listeners
-eventListeneres();
-function eventListeneres() {
+// call event listeners
+eventListeners();
+
+function eventListeners() {
     // submit event
     form.addEventListener('submit', addNewItem);
 
@@ -19,60 +23,105 @@ function eventListeneres() {
     btnDeleteAll.addEventListener('click', deleteAllItems);
 }
 
-// add new event
-function addNewItem(e) {
-    if(input.value ===''){
-        alert('add new item');
-    }
+function loadItems() {
 
+    items = getItemsFromLocalStorage();
+    items.forEach(function (item) {
+        createItem(item);
+    })
+}
+
+// get Items From local storage
+function getItemsFromLocalStorage() {
+    if(localStorage.getItem('items') === null) {
+        items = [];
+    
+    } else {
+        items = JSON.parse(localStorage.getItem('items'));
+    }
+    return items;
+}
+
+// set Items to local storage
+function setItemToLocalStorage(text) {
+    items = getItemsFromLocalStorage();
+    items.push(text);
+    localStorage.setItem('items', JSON.stringify(items));
+
+}
+
+// delete item from local storage
+function deleteItemFromLocalStorage(text) {
+    items = getItemsFromLocalStorage();
+    items.forEach(function(item, index){
+        if(item === text){
+            items.splice(index,1);
+        }
+    });
+    localStorage.setItem('items', JSON.stringify(items));
+}
+
+function createItem(text) {
     // create li
     const li = document.createElement('li');
     li.className = 'list-group-item list-group-item-secondary';
-    li.appendChild(document.createTextNode(input.value));
+    li.appendChild(document.createTextNode(text));
 
     // create a
     const a = document.createElement('a');
-    a.classList='delete-item float-end';
+    a.classList = 'delete-item float-end';
     a.setAttribute('href', '#');
     a.innerHTML = '<i class="fas fa-times"></i>';
 
     // add a to li
     li.appendChild(a);
 
-
     // add li to ul
     taskList.appendChild(li);
 
-    //clear input
+}
+
+// add new item
+function addNewItem(e) {
+    if (input.value === '') {
+        alert('add new item');
+    }
+
+    // create item
+    createItem(input.value);
+
+    //save to local storage
+    setItemToLocalStorage(input.value);
+
+    // clear input
     input.value = '';
-    
+
     e.preventDefault();
+
 }
 
 // delete an item
 function deleteItem(e) {
-    if(e.target.className=='fas fa-times'){
-        e.target.parentElement.parentElement.remove();
+    if (e.target.className === 'fas fa-times') {
+        if (confirm('are you sure ?')) {
+            e.target.parentElement.parentElement.remove();
+
+            //delete item from local storage
+            deleteItemFromLocalStorage(e.target.parentElement.parentElement.textContent); 
+        }
     }
-
     e.preventDefault();
-
 }
 
 // delete all items
-function deleteAllItems(e){
+function deleteAllItems(e) {
 
-    if(confirm('Are you sure?')){
-        //taskList.innerHTML = '';
-        taskList.childNodes.forEach(function(item){
-            if(item.nodeType===1){
-                item.remove();
-            }
-        });
-
-    
+    if (confirm('are you sure ?')) {
+        // taskList.innerHTML='';
+        while(taskList.firstChild) {
+            taskList.removeChild(taskList.firstChild);
+        }
+        localStorage.clear();
     }
-  
     e.preventDefault();
-
 }
