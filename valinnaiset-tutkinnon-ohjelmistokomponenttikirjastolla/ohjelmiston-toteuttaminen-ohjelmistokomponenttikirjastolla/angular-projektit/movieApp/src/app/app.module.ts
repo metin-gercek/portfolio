4cbf3ea38ff36d759266ca6dfb89326e81ac9aee
metin-gercek <metin.gercek@edu.bc.fi>
2022-01-01 15:44:45 +0200

    Movies app creating components

--- valinnaiset-tutkinnon-ohjelmistokomponenttikirjastolla/ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-projektit/movieApp/src/app/app.module.ts
+++ valinnaiset-tutkinnon-ohjelmistokomponenttikirjastolla/ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-projektit/movieApp/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesComponent
+    MoviesComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule
