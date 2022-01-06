4dcc0b9580ab39f0fb3e0c0a56e3262cde4e3e76
metin-gercek <metin.gercek@edu.bc.fi>
2022-01-06 19:05:31 +0200

    create a new project as an exercises named free angular project

---
+++ valinnaiset-tutkinnon-ohjelmistokomponenttikirjastolla/ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-projektit/free-angular-project/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ProductComponent } from './product.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProductComponent
+    
+  
+    
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [ProductComponent]
+})
+export class AppModule { }
---
+++ valinnaiset-tutkinnon-ohjelmistokomponenttikirjastolla/ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-projektit/free-angular-project/src/app/datasource.model.ts
@@ -0,0 +1,20 @@
+import { Product } from "./product.model";
+
+
+export class SimpleDataSource {
+    private products!: Product[];
+
+    constructor() {
+        this.products = new Array<Product>(
+            new Product(1, "Samsung S21", "good phone", "1.jpg", 1500),
+            new Product(2, "Samsung Z", "good phone", "2.jpg", 1200),
+            new Product(3, "Samsung Galaxy Note", "good phone", "3.jpg", 1000),
+            new Product(4, "Samsung A", "good phone", "4.jpg", 1100),
+            new Product(5, "Samsung M", "good phone", "5.jpg", 900),
+        )
+    }
+
+    getProducts(): Product[] {
+        return this.products;
+    }
+}
