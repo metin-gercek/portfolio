a5d005d29f53781e2212230f72007471734431c2
metin-gercek <metin.gercek@edu.bc.fi>
2022-01-07 23:07:00 +0200

    added two way biding  method

--- valinnaiset-tutkinnon-ohjelmistokomponenttikirjastolla/ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-projektit/free-angular-project/src/app/app.module.ts
+++ valinnaiset-tutkinnon-ohjelmistokomponenttikirjastolla/ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-projektit/free-angular-project/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { ProductComponent } from './product.component';
 
@@ -12,7 +13,8 @@ import { ProductComponent } from './product.component';
     
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [ProductComponent]
--- valinnaiset-tutkinnon-ohjelmistokomponenttikirjastolla/ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-projektit/free-angular-project/src/app/product.component.ts
+++ valinnaiset-tutkinnon-ohjelmistokomponenttikirjastolla/ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-projektit/free-angular-project/src/app/product.component.ts
@@ -4,55 +4,61 @@ import { ProductRepository } from './repository.model';
 
 @Component({
   selector: 'app',
-  templateUrl: './product.component.html',
+  template: `
+  <input [value] = "email" (keyup.enter)="email= $any($event.target).value; onKeyUp()"/>
+  <input [(ngModel)]="email" (keyup.enter)="onKeyUp()"/>
+  `,
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
 
   model: ProductRepository = new ProductRepository();
-  disabled = false;
-  constructor() { }
 
-  getClasses(id: number) : string {
-    let product = this.model.getProductById(id);
-    return (product.price <= 1000 ? "bg-info text-white": "bg-secondary") + " m-2 p-2";
-  }
-
-  product: Product = this.model.getProductById(1);
-
-  getClassMap(id: number): Object {
-    let product = this.model.getProductById(id);
-    return {
-      "bg-info": product.price <=1000,
-      "bg-secondary": product.price > 1000,
-      "text-center text-white": product.name == "Samsung Z"
-    }
-  }
-
-  color: string = this.model.getProductById(1).price <= 1000? "red": "blue";
-  fontSize: string = "25px";
-
-  getStyles(id: number) {
-    let product = this.model.getProductById(id);
-    return {
-      fontSize: "25px",
-      color: product.price <= 1000 ? "green" : "red"
-    }
-  }
-
-  onSubmit($event: any) {
-    $event.stopPropagation();
-    console.log('button was clicked');
-    console.log($event);
-  }
-  onDivClicked() {
-    console.log('div was clicked');
-  }
-
-  values = '';
-  onKeyUp(email: any)  {
+  email = "email@gmail.com";
+  onKeyUp()  {
     // this.values += event.target.value;
-      console.log(email);
+      console.log(this.email);
     
   }
+  // disabled = false;
+  // constructor() { }
+
+  // getClasses(id: number) : string {
+  //   let product = this.model.getProductById(id);
+  //   return (product.price <= 1000 ? "bg-info text-white": "bg-secondary") + " m-2 p-2";
+  // }
+
+  // product: Product = this.model.getProductById(1);
+
+  // getClassMap(id: number): Object {
+  //   let product = this.model.getProductById(id);
+  //   return {
+  //     "bg-info": product.price <=1000,
+  //     "bg-secondary": product.price > 1000,
+  //     "text-center text-white": product.name == "Samsung Z"
+  //   }
+  // }
+
+  // color: string = this.model.getProductById(1).price <= 1000? "red": "blue";
+  // fontSize: string = "25px";
+
+  // getStyles(id: number) {
+  //   let product = this.model.getProductById(id);
+  //   return {
+  //     fontSize: "25px",
+  //     color: product.price <= 1000 ? "green" : "red"
+  //   }
+  // }
+
+  // onSubmit($event: any) {
+  //   $event.stopPropagation();
+  //   console.log('button was clicked');
+  //   console.log($event);
+  // }
+  // onDivClicked() {
+  //   console.log('div was clicked');
+  // }
+
+  // values = '';
+  
 }
