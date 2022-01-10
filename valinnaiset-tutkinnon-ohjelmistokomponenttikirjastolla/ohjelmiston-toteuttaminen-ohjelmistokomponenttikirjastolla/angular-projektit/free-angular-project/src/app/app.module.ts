d03d0025de89cf0340a671e0419853196fd831ae
metin-gercek <metin.gercek@edu.bc.fi>
2022-01-10 21:37:28 +0200

    added custom directive

--- valinnaiset-tutkinnon-ohjelmistokomponenttikirjastolla/ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-projektit/free-angular-project/src/app/app.module.ts
+++ valinnaiset-tutkinnon-ohjelmistokomponenttikirjastolla/ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-projektit/free-angular-project/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { ProductComponent } from './product.component';
+import { InputEmailDirective } from './input-email.directive';
 
 
 
 @NgModule({
   declarations: [
-    ProductComponent
+    ProductComponent,
+    InputEmailDirective
     
   
     
