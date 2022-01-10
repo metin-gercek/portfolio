e059260ca09c3b5dd9803508541e3df05c2e9f70
metin-gercek <metin.gercek@edu.bc.fi>
2022-01-10 13:59:55 +0200

    added ngIf

--- valinnaiset-tutkinnon-ohjelmistokomponenttikirjastolla/ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-projektit/free-angular-project/src/app/product.component.ts
+++ valinnaiset-tutkinnon-ohjelmistokomponenttikirjastolla/ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-projektit/free-angular-project/src/app/product.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { Product } from './product.model';
+
 import { ProductRepository } from './repository.model';
 
 @Component({
   selector: 'app',
-  template: `
-  <input [value] = "email" (keyup.enter)="email= $any($event.target).value; onKeyUp()"/>
-  <input [(ngModel)]="email" (keyup.enter)="onKeyUp()"/>
-  `,
+  templateUrl: 'product.component.html',
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
