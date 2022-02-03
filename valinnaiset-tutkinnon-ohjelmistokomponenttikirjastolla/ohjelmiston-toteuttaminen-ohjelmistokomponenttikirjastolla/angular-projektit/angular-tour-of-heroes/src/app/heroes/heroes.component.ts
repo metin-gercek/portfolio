af701ebc47428aae9baa748812556149e253de4a
metin-gercek <metin.gercek@edu.bc.fi>
2022-02-03 10:31:08 +0200

    add hero service

--- valinnaiset-tutkinnon-ohjelmistokomponenttikirjastolla/ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-projektit/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ valinnaiset-tutkinnon-ohjelmistokomponenttikirjastolla/ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-projektit/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+
+import { HeroService } from '../hero.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -8,18 +10,21 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Windstorm'
-  // }
-  heroes = HEROES;
-  constructor() { }
+ 
+  heroes: Hero[] = [];
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
 }
